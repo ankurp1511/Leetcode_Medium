300673e4643f94a359b8d8fb626cc8a7ab71841b
ankurp1511 <ankur.b2k12.iiita@gmail.com>
2022-04-08 23:38:37 +0530

    Leetcode_695: solution using BFS

---
+++ Leetcode_695/src/Solution.java
@@ -0,0 +1,82 @@
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Solution {
+
+    static class Node {
+        int val;
+        int row;
+        int column;
+
+        public int getVal() {
+            return val;
+        }
+
+        public int getRow() {
+            return row;
+        }
+
+        public int getColumn() {
+            return column;
+        }
+
+        public Node(int val, int row, int column) {
+            this.val = val;
+            this.row = row;
+            this.column = column;
+        }
+    }
+
+    static int[][] directions = new int[][] {
+            {-1,0},{1,0},{0,-1},{0,1}
+    };
+
+    public static void main(String[] args) {
+        int[][] grid = new int[][]{
+                {0,0,1,0,0,0,0,1,0,0,0,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,1,1,0,1,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,1,0,0,1,0,1,0,0},
+                {0,1,0,0,1,1,0,0,1,1,1,0,0},
+                {0,0,0,0,0,0,0,0,0,0,1,0,0},
+                {0,0,0,0,0,0,0,1,1,1,0,0,0},
+                {0,0,0,0,0,0,0,1,1,0,0,0,0}
+        };
+
+        int maxAreaIsland = 0;
+
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+
+        for (int i=0; i< grid.length; i++) {
+            for (int j=0; j<grid[0].length; j++) {
+                if (grid[i][j] == 1 && !visited[i][j]) {
+                    maxAreaIsland = Math.max(maxAreaIsland, bfs(i, j, grid, visited));
+                }
+            }
+        }
+        System.out.println(maxAreaIsland);
+    }
+
+    private static int bfs(int i, int j, int[][] grid, boolean[][] visited) {
+
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(grid[i][j], i, j));
+        visited[i][j] = true;
+        int count = 1;
+
+        while (!queue.isEmpty()) {
+            Node node = queue.poll();
+            for (int[] arr: directions) {
+                if (node.row + arr[0] >= 0 && node.row + arr[0] < grid.length
+                        && node.column + arr[1] >= 0 && node.column + arr[1] < grid[0].length
+                        && !visited[node.row + arr[0]][node.column + arr[1]]
+                        && grid[node.row + arr[0]][node.column + arr[1]] == 1) {
+                    queue.add(new Node(grid[node.row + arr[0]][node.column + arr[1]], node.row + arr[0], node.column + arr[1]));
+                    visited[node.row + arr[0]][node.column + arr[1]] = true;
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
